b193ddb383fe63dff63ca6b32f44406b173f17ed
Carl Poole <mail@carlpoole.com>
2014-05-01 01:06:42 -0400

    Functional now

--- main.asm
+++ main.asm
@@ -1,26 +1,29 @@
-TITLE Calculator				(main.asm)
+TITLE Calculator Program - CARL POOLE 5/1/14 (main.asm)
 INCLUDE Irvine32.inc
+
+ENTER_KEY = 13
+
 .data 
 
 ; --------------------------------------
 ; Places to store operands and operator
 ; --------------------------------------
 
-operString	DWORD 30 dup(0)
-val1		DWORD ?
-val2		DWORD ?
-oper		BYTE  ?
-result		DWORD ?
+variableA	DWORD 0
+variableB	DWORD 0
+neg1		BYTE  0
+neg2		BYTE  0
+errflag		BYTE  0
 
 ; ---------------
 ; Message Strings
 ; ---------------
 
 welcome		BYTE '-- Carls Calculator --',0
-example		BYTE 'Enter your operation (E.G.): -12 + 34 or 1455 * 2',0
-illegalChar BYTE 'Invalid Input Char',0
-illegalOper BYTE 'Must enter + - * / % only',0
-divbyzero	BYTE 'Cannot div by zero',0
+example		BYTE 'Enter your operation (E.G.): -12+34 or 1455*2',0
+illegalChar BYTE '!! Invalid Input Char !!',0
+divbyzero	BYTE '!! Cannot div by zero !!',0
+ofErrorSt	BYTE '!! Overflow Error !!',0
 
 .code 
 
@@ -29,7 +32,7 @@ main PROC
 	; ------------------------
 	; App Welcome and Example
 	; ------------------------
-	MOV eax, 0
+
 	MOV edx, OFFSET welcome
 	CALL writestring
 	CALL CrLf
@@ -38,203 +41,253 @@ main PROC
 	CALL CrLf
 	CALL CrLf
 
+	START:			; App loops back here after calculating
+	MOV eax, 0
+	MOV ebx, 0
+
 	; ------------------------
-	; Read Operation String
+	; Process Input FSM
 	; ------------------------
 
-	MOV ecx, lengthOf operString
-	MOV edx, offset operString
-	CALL readString
+	StateA: ; read in one char at a time, continue reading until get a sign 
 
-	; ------------
-	; FSM Parsing
-	; ------------
+	 mov eax, 0
+	 call ReadChar 
 
-	MOV esi, OFFSET operString
+	 cmp al, '-'		; check if a negative symbol was first
+	 jnz NOTNEGATIVE
+	 MOV neg1, 1		; Flag neg1 to make sure to negate later
+	 call ReadChar
 
-	; Do initial test to see if negative
-	;	or digit begins the string
+	 NOTNEGATIVE:
+	 call IsDigit		;check to see if the value entered was a digit 
+	 jnz CHARERR 
+	 sub al, '0' 
+	 MOVZX eax, al
+	 mov variableA, eax 
+	 jmp StateB 
 
-	MOV eax, [esi]
-	call IsDigit
-	jnz CHARERR
+	StateB:				; check first if a valid operator was entered
+	 mov eax, 0 
+	 call ReadChar 
+	 cmp al, '+' 
+	 jnz C1 
+	 mov ecx, addition 
+	 jmp StateC 
+	 C1:
+	 cmp al, '-' 
+	 jnz C2 
+	 mov ecx, subtraction 
+	 jmp StateC 
+	 C2:
+	 cmp al, '*' 
+	 jnz C3 
+	 mov ecx, multiplication 
+	 jmp StateC 
+	 C3:
+	 cmp al, '/' 
+	 jnz C4 
+	 mov ecx, division 
+	 jmp StateC 
+	 C4:
+	 cmp al, '%' 
+	 jnz C5 
+	 mov ecx, modulo 
+	 jmp StateC 
+	 C5:
+		jmp L1
 
-	MOV bx, 0 ; counter for 
+ 	L1: 
+	 call IsDigit		; check to see if the value entered was a digit 
+	 jnz CHARERR 
+	 sub al, '0'
+	 MOVZX eax, al 
+	 mov ebx, eax 
+	 mov eax, 10 
+	 imul variableA 
+	 jo OVFERR 
+	 mov variableA, eax
+	 add variableA, ebx		; variableA = variableA * 10 + ebx 
+	 jmp StateB				; jump back and check if operator entered yet
 
-	READFIRST:
-		MOV eax, [esi]
+	StateC:
+	 mov eax, 0
+	 call ReadChar 
 
-		; Check if space
-		;	if so, jump to read operator
+	 cmp al, '-'		; check if a negative symbol was first and flag if so
+	 jnz NOTNEGATIVE2
+	 MOV neg2, 1
+	 call ReadChar
 
-		cmp al, 32
-		je READOPER
+	 NOTNEGATIVE2:
+	 call IsDigit		; check to see if the value entered was a digit 
+	 jnz CHARERR 
+	 sub al, '0' 
+	 MOVZX eax, al
+	 mov variableB, eax 
+	 jmp StateD 
 
-		; Check if digit
-		;	if not, 
+	StateD:				; continues reading second number until enter key
 
-		call IsDigit
-		jnz CHARERR
+	 mov eax, 0 
+	 call ReadChar 
+	 cmp al, ENTER_KEY 
+	 jz Done 
+	 call isDigit 
+	 jnz CHARERR 
+	 sub al, '0' 
+	 MOVZX eax, al
+	 mov ebx, eax 
+	 mov eax, 10 
+	 imul variableB 
+	 jo OVFERR 
+	 mov variableB, eax 
+	 add variableB, ebx
+	 jmp StateD 
 
-		; Push number char onto stack
-		PUSH eax
-		inc bx
+	Done: 
 
-		inc esi
-		loop READFIRST
+	 cmp neg1, 1			; if any operators were flagged negative
+	 jnz SKIPNEG1
+	 MOV eax, variableA
+	 NEG eax				; Negate them
+	 MOV variableA, eax
 
-	READOPER:
+	 SKIPNEG1:
 
-		PUTVAL1:
-			MOV eax, 0
-			POP eax
-			MOV val1, eax
-			call writeInt
+	 cmp neg2, 1
+	 jnz SKIPNEG2
+	 MOV eax, variableB
+	 NEG eax
+	 MOV variableB, eax
 
-		inc esi
-		MOV eax, [esi]
+	 SKIPNEG2:
 
-		cmp al, '+'
-			je GOODOPER
-		cmp al, '-'
-			je GOODOPER
-		cmp al, '/'
-			je GOODOPER
-		cmp al, '*'
-			je GOODOPER
-		cmp al, '%'
-			je GOODOPER
 
-		jmp CHARERR 
+	 call ecx				; run the calculation
 
-	GOODOPER:
-		MOV oper, al
+	 cmp errflag, 1			; if error printed skip printing answer (wrong anyway)
+	 jz AGAIN
 
-	READSECOND:
-		ADD esi, 2
-		SUB ecx, 2
+	 call writeInt			; print answer
 
-		; Check if nothing was given for 2nd
+	 JMP AGAIN				
 
-		MOV eax, [esi]
-		cmp al, 0
-		je CHARERR
-		cmp al, 10
-		je CHARERR
-
-		READSECOND_1:
-			MOV eax, [esi]
-
-			; Check if end reached
-			cmp al, 0
-			je DONE
-			cmp al, 10
-			je DONE
-
-			; Check if digit
-			;	if not, 
-
-			call IsDigit
-			jnz CHARERR
-
-			inc esi
-			loop READSECOND_1
-
-	DONE:
-
-	; ----------------
-	; Execute operand
-	; ----------------
-
-	MOV al, oper
-
-	PLUSOPER:
-		cmp al, '+'
-		jne MINOPER
-		call addition
-		jmp FINISHUP
-	MINOPER:
-		cmp al, '-'
-		jne DIVOPER
-		call subtraction
-		jmp FINISHUP
-	DIVOPER:
-		cmp al, '/'
-		jne MULTOPER
-		call division
-		jmp FINISHUP
-	MULTOPER:
-		cmp al, '*'
-		jne MODOPER
-		call multiplication
-		jmp FINISHUP
-	MODOPER:
-		call modulo
-
-	FINISHUP:
-
-		MOV al, '='
-		call writeChar
-		MOV al, ' '
-		call writeChar
-		MOV eax, result
-		call writeInt
-
-	EXIT
+	 ; ----- Error handling section is here --------
 
 	CHARERR:
 		CALL errChar
+		JMP AGAIN
 
 	DIV0ERR:
 		CALL errDiv0
+		JMP AGAIN
+
+	OVFERR:
+		CALL ofError
+		JMP AGAIN
+
+	; -----------------------------------------------
+
+	AGAIN:					; loop back to start to do another calculation
+	 MOV  errflag, 0
+	 call CrLf
+	 call CrLf
+	 jmp START
+
+	 EXIT
 
 main ENDP
 
 errChar PROC
-
 	MOV edx, OFFSET illegalChar
 	CALL CrLf
 	CALL writestring 
-
-	EXIT
+	RET
 errChar ENDP
 
 errDiv0 PROC
-
 	MOV edx, OFFSET divbyzero
 	CALL CrLf
 	CALL writestring 
-
-	EXIT
+	RET
 errDiv0 ENDP
 
+ofError PROC
+	MOV edx, OFFSET ofErrorSt
+	CALL CrLf
+	CALL writestring 
+	RET
+ofError ENDP
+
 addition PROC
-
-	MOV eax, val1
-	ADD eax, val2
-	MOV result, eax
-
+	MOV eax, variableA
+	ADD eax, variableB
+	jno done 
+	CALL ofError
+	MOV  errflag, 1
+done: 
 	RET
 addition ENDP
 
 subtraction PROC
-
-	MOV eax, val1
-	SUB eax, val2
-	MOV result, eax
-
+	MOV eax, variableA
+	SUB eax, variableB
+	jno done 
+	CALL ofError
+	MOV  errflag, 1
+done: 
 	RET
 subtraction ENDP
 
 multiplication PROC
+	MOV eax, variableA
+	MOV ebx, variableB
+	CDQ
+	IMUL ebx
+	jno done 
+	CALL ofError
+	MOV  errflag, 1
+done: 
 	RET
 multiplication ENDP
 
 division PROC
+
+	MOV eax, variableA
+	CDQ
+	MOV ebx, variableB
+	CDQ
+	CMP ebx, 0
+	JE DIV0
+	IDIV ebx
+	jno done 
+	CALL ofError
+	MOV  errflag, 1
+done: 
+	RET
+
+DIV0: 
+	MOV  errflag, 1
+	CALL errDiv0
 	RET
 division ENDP
 
 modulo PROC
+
+	MOV eax, variableA
+	CDQ
+	MOV ebx, variableB
+	CDQ
+	CMP ebx, 0
+	JE DIV0
+	IDIV ebx
+	MOV eax, edx
+	RET
+
+DIV0: 
+	MOV  errflag, 1
+	CALL errDiv0
 	RET
 modulo ENDP
 
